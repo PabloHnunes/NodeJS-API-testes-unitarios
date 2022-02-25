b4aee9784c38cec6e496743dca6ca9bc4510b507
Pablo Henrique Nunes <pablo.nunes@systextil.com.br>
2022-02-25 20:34:26 -0300

    adiciona novos testes da função base de captura de urls

--- index.js
+++ index.js
@@ -1,67 +1,47 @@
 import chalk from "chalk";
-import fs from 'fs';
+import fs from "fs";
 import path from "path";
 
 const log = console.log;
-const arquivoUrl = './arquivos/texto1.md';
 
-function extraiTexto(texto){
-    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
-    const arrayResultados = [];
-    let temp;
-    while((temp = regex.exec(texto)) !== null){
-        arrayResultados.push({[ temp[1]]: temp[2]});
-    }
-    return arrayResultados.length === 0 ? chalk.red('Não há links') : arrayResultados;
+function extraiTexto(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  return arrayResultados.length === 0 ? "Não há links" : arrayResultados;
 }
 
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho!'));
-};
-
-export default async function pegaArquivo(caminho){
-    const caminhoAbsoluto = path.join(path.resolve(), '.', caminho);
-    const encoding = 'utf-8';
-    const arquivos = await fs.promises.readdir(caminhoAbsoluto,{ encoding });
-
-    const result = await Promise.all(arquivos.map(async (arquivo) => {
-        const localArquivo = `${caminhoAbsoluto}/${arquivo}`;
-        const texto = await fs.promises.readFile(localArquivo, encoding);
-        return extraiTexto(texto);
-      }));
-
-    return result;
+  throw new Error(chalk.red(erro.code, "não há arquivo no caminho!"));
 }
-// export async function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     try{
-//         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-//         return extraiTexto(texto);
-//     } catch (erro) {
-//         trataErro(erro)
-//     }
-//     finally{
-//         log(chalk.yellow("Comportamento async executado com sucesso!"));
-//     }
 
-// }
+export async function pegaArquivo(caminho) {
+  const caminhoAbsoluto = path.join(path.resolve(), ".", caminho);
+  const encoding = "utf-8";
+  const arquivos = await fs.promises.readdir(caminhoAbsoluto, { encoding });
 
-/* Função usando o .then() para assincrono  */
-// function pegaArquivo( caminhoDoArquivo ){
-//     const encoding = 'utf-8';
-//         fs.promises.readFile(caminhoDoArquivo, encoding)
-//         .then((response) => { log(chalk.green(response)) })
-//         .catch((erro) => { trataErro(erro)})
-// }
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, data) => {
-//         if(erro){
-//             trataErro(erro);
-//         }else{
-//             log(chalk.green(data));
-//         };
-//     })
-// };
+  const result = await Promise.all(
+    arquivos.map(async (arquivo) => {
+      const localArquivo = `${caminhoAbsoluto}/${arquivo}`;
+      const texto = await fs.promises.readFile(localArquivo, encoding);
+      return extraiTexto(texto);
+    })
+  );
 
-//pegaArquivo(arquivoUrl);
+  return result;
+}
+
+export async function pegaArquivoTest(caminhoDoArquivo) {
+  const encoding = "utf-8";
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    return extraiTexto(texto);
+  } catch (erro) {
+    trataErro(erro);
+  } finally {
+    log(chalk.yellow("Comportamento async executado com sucesso!"));
+  }
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,31 @@
-import pegaArquivo from "..";
+import { pegaArquivoTest } from "..";
 
 const arrayResult = [
-  [
     {
       FileList: `https://developer.mozilla.org/pt-BR/docs/Web/API/FileList`,
     },
-  ]
 ];
 
-describe("pegaArquivo::", () => {
+describe("pegaArquivoTest::", () => {
   it("deve ser uma função", () => {
-    expect(typeof pegaArquivo).toBe("function");
+    expect(typeof pegaArquivoTest).toBe("function");
   });
 
   it("deve retornar array com resultados", async () => {
-    const result = await pegaArquivo("./test/arquivos/");
+    const result = await pegaArquivoTest("./test/arquivos/texto1.md");
     expect(result).toEqual(arrayResult);
+  });
+
+  it("deve retornar mensagem 'Não há links' ", async () => {
+      const result = await pegaArquivoTest("./test/arquivos/texto2.md");
+
+      expect(result).toBe("Não há links");
+  });
+
+  it('deve lançar um erro na falta de arquivo', async () => {
+    await expect(pegaArquivoTest('./test/arquivos_test/')).rejects.toThrow(/não há arquivo no caminho/)
+  });
+  it('deve resolver a função com sucesso', async () => {
+    await expect(pegaArquivoTest('./test/arquivos/texto1.md')).resolves.toEqual(arrayResult)
   });
 });
